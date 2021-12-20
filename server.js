50042dbe5bd75ca06ff0e643febfc0fc989bece8
عبدالله الصبي <Abdullahb.alsabi@gmail.com>
2021-12-20 14:58:00 +0300

    deployment to herocu

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = 3001;
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const usersRouter = require("./routers/UserRoutes");
@@ -30,11 +31,15 @@ app.use("/users", usersRouter);
 app.use("/portfolio", portfolioRouter);
 app.use("/transactions", TransactionRoutes);
 app.use("/admin", AdminRoutes);
+app.use(express.static("frontend/build"));
+
 
 app.use(notFound);
 app.use(errorHandler);
 // app.use("/authors", authorsRouter);
-
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
+});
 app.listen(PORT, () => {
   console.log(`Connected on= http://localhost:${PORT}`);
 });
