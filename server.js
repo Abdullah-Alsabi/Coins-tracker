05c0d2dabd82a4801710431883a79cbe25d20826
عبدالله الصبي <Abdullahb.alsabi@gmail.com>
2021-12-26 09:46:25 +0300

    heroku deployment

--- server.js
+++ server.js
@@ -32,18 +32,27 @@ app.use("/portfolio", portfolioRouter);
 app.use("/transactions", TransactionRoutes);
 app.use("/admin", AdminRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+app.use(express.static("frontend/build"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
+// });
+
 app.use(notFound);
 app.use(errorHandler);
 // app.use("/authors", authorsRouter);
 
-app.use(express.static("frontend/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
-});
+//
 
-if(process.env.NODE_ENV=="production"){
- app.use(express.static("frontend/build"));
-
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("frontend/build"));
 }
 app.listen(PORT, () => {
   console.log(`Connected on= http://localhost:${PORT}`);
