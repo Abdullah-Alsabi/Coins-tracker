7d70b7e4211f4ffb8b44abc5e3d0e41c3350d81e
عبدالله الصبي <Abdullahb.alsabi@gmail.com>
2021-12-26 13:04:14 +0300

    deployment

--- server.js
+++ server.js
@@ -42,14 +42,14 @@ app.use("/admin", AdminRoutes);
 app.use(errorHandler);
 // app.use("/authors", authorsRouter);
 
-// app.use(express.static("frontend/build"));
+app.use(express.static("frontend/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
 });
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("frontend/build"));
-}
+// if (process.env.NODE_ENV == "production") {
+//   app.use(express.static("frontend/build"));
+// }
 app.listen(PORT, () => {
   console.log(`Connected on= http://localhost:${PORT}`);
 });
